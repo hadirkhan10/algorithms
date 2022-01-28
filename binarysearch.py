5267c5285ba961ce6a135768e760f9d6d664da8e
Hadir Khan <hadirkhan10@gmail.com>
2022-01-28 08:23:38 -0800

    updated with better printing

--- binarysearch.py
+++ binarysearch.py
@@ -10,6 +10,9 @@ def binarySearch(arr, targetVal):
             return -1
         guess = int((min + max)/2)
         if arr[guess] == targetVal:
+            print("We are on iter: " + str(iter))
+            print("The guess is " + str(arr[guess]))
+            print("we found it!") 
             return guess
         elif arr[guess] < targetVal:
             min = guess + 1
@@ -27,5 +30,5 @@ def binarySearch(arr, targetVal):
 values = [10, 20, 24, 25, 30, 56, 67, 68, 69, 73, 78, 80, 100, 140, 200, 205]
 
 # the output will be the index where the target value is found
-result = binarySearch(values, 300)
-print(result)
+result = binarySearch(values, 20)
+print("the index of target value is: " + str(result))
